91c33ee6227393c1674ffccaa9d3462264466f0b
10-abhi <abhishek81936@gmail.com>
2025-03-21 15:45:19 +0530

    resolving deployment err

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -63,4 +63,8 @@ app.post("/hdfcWebhook" , async (req,res)=>{
     }
 })
 
-app.listen(3001);
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
