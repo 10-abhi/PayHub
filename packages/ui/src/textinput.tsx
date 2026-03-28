bf68aeaf1cc1236dc0261cddbe2eb0d444d10a2e
10-abhi <abhishek81936@gmail.com>
2026-03-28 16:26:36 +0530

    some minor changes

--- packages/ui/src/textinput.tsx
+++ packages/ui/src/textinput.tsx
@@ -10,7 +10,7 @@ export const TextInput = ({
     label: string;
 }) => {
     return <div className="pt-2">
-        <label className="block mb-2 text-sm font-medium text-gray-900">{label}</label>
-        <input onChange={(e) => onChange(e.target.value)} type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} />
+        <label className="block mb-2 text-sm font-medium text-slate-400">{label}</label>
+        <input onChange={(e) => onChange(e.target.value)} type="text" className="bg-slate-950 border border-slate-700 text-slate-200 text-sm rounded-xl focus:ring-emerald-500 focus:border-emerald-500 block w-full p-2.5 placeholder-slate-500 outline-none transition-colors duration-200" placeholder={placeholder} />
     </div>
 }
